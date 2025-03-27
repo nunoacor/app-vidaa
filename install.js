6dac26465a38c2890c378150ccae4776ee6d4b61
PhasedGapple <alland2301@gmail.com>
2025-03-27 12:35:33 +0100

    Update install.js
    Made EaglerCraftX 1.8.8 installable

--- install.js
+++ install.js
@@ -13,6 +13,21 @@ function hax() {
     Hisense_installApp(appId, appName, thumbnail, iconSmall, iconBig, AppUrl, storetype, installCallBack);
 }
 
+function eaglercraft() {
+    // Custom icon
+    var thumb = "https://atlas-content-cdn.pixelsquid.com/stock-images/file-folder-mdmADN9-600.jpg";
+    var appName = "EaglerCtaftX 1.8.8";
+
+    var thumbnail = thumb;
+    var iconSmall = thumb;
+    var iconBig = thumb;
+    var AppUrl = "https://eaglercraft.com/mc/1.8.8/";
+    var storetype = "store";
+    var appId = appName;
+
+    Hisense_installApp(appId, appName, thumbnail, iconSmall, iconBig, AppUrl, storetype, installCallBack);
+}
+
 function installCallBack(res) {
     if (res == 0) {
         showPopup("Success", "The app was installed successfully.");
